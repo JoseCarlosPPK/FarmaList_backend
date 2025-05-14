ed15fbc0184698a58209565ffa11c48c33ef5e4e
JoseCarlos <jcarloslopez@correo.ugr.es>
2025-05-14 19:07:19 +0200

    ReAdd /farmacias endpoint
    Go on #10

--- flask_app/API/centros/__init__.py
+++ flask_app/API/centros/__init__.py
@@ -1,4 +1,8 @@
 from flask_restful import Api
-from .. import app
+from .. import app,BASE_API
+from .farmacia import Farmacia
 
 api = Api(app)
+
+# Registro de las APIs 
+api.add_resource(Farmacia, f"{BASE_API}/farmacias", f"{BASE_API}/farmacias/<int:id>")
