fb7dda37c8096c7e1074d064f3abbe6c1adb369d
JoseCarlos <jcarloslopez@correo.ugr.es>
2025-06-16 13:25:59 +0200

    Add DELETE /convocatorias
    Go on #18

--- flask_app/API/convocatorias.py
+++ flask_app/API/convocatorias.py
@@ -18,3 +18,20 @@ def api_get_convocatorias():
 
 
 
+@app.delete(f"{BASE_API}/convocatorias/<int:id>")
+@jwt_required()
+def api_delete_convocatoria(id):
+   a_borrar = Convocatoria.query.get(id)
+
+   if (not a_borrar):
+      return {"error": f"No hay ninguna convocatoria con id {id}"}, 404
+
+   try:
+      db.session.delete(a_borrar)
+      db.session.commit()
+   except Exception as e:
+      return {"error": f"Fallo del servidor al realizar el borrado de la convocatoria con id {id}"}, 500
+
+   return {"msg": f"Centro con id {id} borrado"}, 200 # https://developer.mozilla.org/en-US/docs/Web/HTTP/Reference/Methods/DELETE
+
+
